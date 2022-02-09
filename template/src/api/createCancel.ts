5542f255b26cfb94aba760c995d9d7436361507e
yuzhiyong <yuzhiyong@newrank.cn>
2022-02-09 09:19:38 +0800

    wip: 小修改axios取消连续请求备注

--- template/src/api/createCancel.ts
+++ template/src/api/createCancel.ts
@@ -1,6 +1,9 @@
+/**
+ * 取消连续请求, 默认关闭, 需要开启时需要再请求接口那里把[config]的params: { cancle: true }打开, 但有个问题是params配置会在url后带上cancle, 后面看有没可以改的方法
+ */
+
 import axios from 'axios';
 
-// 取消连续请求, 默认关闭, 需要开启时需要再请求接口那里吧[config]的params: { cancle: true }打开, 但有个问题是params配置会在url后带上cancle, 后面看有没可以改的方法
 const createCancel = () => {
   const pending: {
     [key: string]: any;
